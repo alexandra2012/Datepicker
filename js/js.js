9cdc61db1ccabf7f18d20dc2cdd5ec1983301bcb
Stina Jensen <78475642+alexandra2012@users.noreply.github.com>
2021-04-15 12:33:32 +0200

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,16 @@
+
+// JQUERY UI
+// datepicker
+
+$( "#datepicker" ).datepicker({
+  showAnim:"clip",
+  dateFormat:"yy-mm-dd",
+  showOtherMonths: true,
+  selectOtherMonths: true,
+  showOn:"both",
+  buttonImage: "https://www.pinclipart.com/picdir/big/553-5532300_transparent-calender-icon-png-january-calendar-icon-png.png",
+  buttonImageOnly: true,
+  buttonText: "Select date"
+});  
+
+  $('.ui-datepicker-trigger').css('width','17px')
